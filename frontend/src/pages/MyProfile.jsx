c6bd9ca4e6875d4f2a4ebd3a257c45b14805b7b9
ED MARTIN LEGASPI <143507044+ullamae@users.noreply.github.com>
2025-05-05 23:40:10 +0800

    Update MyProfile.jsx

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -56,7 +56,7 @@ const MyProfile = () => {
                     </div>
                     <input onChange={(e) => setImage(e.target.files[0])} type="file" id="image" hidden />
                 </label>
-                : <img className='w-36 rounded' src={userData.image} alt="" />
+                : <img className='w-full h-48 rounded object-cover' src={userData.image} alt="" />
             }
 
             {isEdit
@@ -67,45 +67,45 @@ const MyProfile = () => {
             <hr className='bg-[#ADADAD] h-[1px] border-none' />
 
             <div>
-                <p className='text-gray-600 underline mt-3'>CONTACT INFORMATION</p>
+                <p className='text-gray-600 underline mt-3 text-xl'>CONTACT INFORMATION</p>
                 <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-[#363636]'>
-                    <p className='font-medium'>Email id:</p>
-                    <p className='text-blue-500'>{userData.email}</p>
-                    <p className='font-medium'>Phone:</p>
+                    <p className='font-medium text-lg'>Email id:</p>
+                    <p className='text-blue-500 text-lg'>{userData.email}</p>
+                    <p className='font-medium text-lg'>Phone:</p>
 
                     {isEdit
                         ? <input className='bg-gray-50 max-w-52' type="text" onChange={(e) => setUserData(prev => ({ ...prev, phone: e.target.value }))} value={userData.phone} />
-                        : <p className='text-blue-500'>{userData.phone}</p>
+                        : <p className='text-blue-500 text-lg'>{userData.phone}</p>
                     }
 
-                    <p className='font-medium'>Address:</p>
+                    <p className='font-medium text-lg'>Address:</p>
 
                     {isEdit
-                        ? <input className='bg-gray-50' type="text" onChange={(e) => setUserData(prev => ({ ...prev, address: e.target.value }))} value={userData.address} />
-                        : <p className='text-gray-500'>{userData.address}</p>
+                        ? <input className='bg-gray-50 text-lg' type="text" onChange={(e) => setUserData(prev => ({ ...prev, address: e.target.value }))} value={userData.address} />
+                        : <p className='text-gray-500 text-lg'>{userData.address}</p>
                     }
 
                 </div>
             </div>
             <div>
-                <p className='text-[#797979] underline mt-3'>BASIC INFORMATION</p>
+                <p className='text-[#797979] underline mt-3 text-xl'>BASIC INFORMATION</p>
                 <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-gray-600'>
-                    <p className='font-medium'>Gender:</p>
+                    <p className='font-medium text-lg'>Gender:</p>
 
                     {isEdit
                         ? <select className='max-w-20 bg-gray-50' onChange={(e) => setUserData(prev => ({ ...prev, gender: e.target.value }))} value={userData.gender} >
-                            <option value="Not Selected">Not Selected</option>
+                            <option value="Not Selected text-lg">Not Selected</option>
                             <option value="Male">Male</option>
                             <option value="Female">Female</option>
                         </select>
-                        : <p className='text-gray-500'>{userData.gender}</p>
+                        : <p className='text-gray-500 text-lg'>{userData.gender}</p>
                     }
 
-                    <p className='font-medium'>Birthday:</p>
+                    <p className='font-medium text-lg'>Birthday:</p>
 
                     {isEdit
                         ? <input className='max-w-28 bg-gray-50' type='date' onChange={(e) => setUserData(prev => ({ ...prev, dob: e.target.value }))} value={userData.dob} />
-                        : <p className='text-gray-500'>{userData.dob}</p>
+                        : <p className='text-gray-500 text-lg'>{userData.dob}</p>
                     }
 
                 </div>
