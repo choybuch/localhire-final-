f25630bdfbc67401e4f12455b224cfcb305fcf0d
ED MARTIN LEGASPI <143507044+ullamae@users.noreply.github.com>
2025-05-06 21:33:36 +0800

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currencySymbol = '$'
+    const currencySymbol = '₱'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [contractors, setContractors] = useState([])
